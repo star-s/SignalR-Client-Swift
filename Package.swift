04893c77d5a8f9940eabcada60abff21a5209476
Sergey Starukhin <star.s@me.com>
2020-11-06 17:26:24 +0100

    rename module 2

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "SignalRClient",
+    name: "SwiftSignalRClient",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SwiftSignalRClient",
-            targets: ["SignalRClient"]),
+            targets: ["SwiftSignalRClient"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,11 +19,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "SignalRClient",
+            name: "SwiftSignalRClient",
             dependencies: []),
         .testTarget(
             name: "SignalRClientTests",
-            dependencies: ["SignalRClient"]),
+            dependencies: ["SwiftSignalRClient"]),
     ],
     swiftLanguageVersions: [.v4, .v4_2, .v5]
 )
